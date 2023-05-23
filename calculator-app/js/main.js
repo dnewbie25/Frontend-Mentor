9a5f228a073035c1aefaea82294bf1d4894ca067
daniel <danielstmontoya@gmail.com>
2023-05-23 14:28:34 -0500

    keyboard input, work in progress

--- calculator-app/js/main.js
+++ calculator-app/js/main.js
@@ -40,6 +40,7 @@ deleteBtn.addEventListener('click',()=>{
 buttons.forEach((number)=>{
   if(number.classList.contains("numbers")){
     number.addEventListener('click',()=>{
+      let cursorPosition = display.selectionStart;
       if(number.id==="dot" && !display.value.includes(".")){
         display.value += ".";
       }else if(number.id!=="dot"){
@@ -50,3 +51,20 @@ buttons.forEach((number)=>{
 });
 
 // keyboard numpad to prevent multiple dots or letters
+
+// re do this one, use selectionStart to typed from the position the cursor is at
+const keyboard = new KeyboardEvent("keydown");
+display.addEventListener("keydown",(e)=>{
+  //e.preventDefault();
+  const numpad = "1234567890/";
+  if(e.key === "."){
+    if(!display.value.includes(".")){
+      let cursorPosition = display.selectionStart;
+      display.value = display.value.slice(0, cursorPosition) + "." + display.value.slice(cursorPosition,display.value.lenth);
+    }else if(display.value.includes(".") && e.key !== "."){
+      display.value += e.key;
+    }
+  }else if(numpad.includes(e.key)){
+    display.value += e.key;
+  }
+})
