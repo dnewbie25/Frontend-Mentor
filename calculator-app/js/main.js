7666ee2be849377b9c736d46389f8a84e03516c9
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2024-03-18 11:25:04 -0500

    single digit calculator working
    needs to further check multiple digit calculations

--- calculator-app/js/main.js
+++ calculator-app/js/main.js
@@ -2,6 +2,9 @@
 
 let numberButtons = document.querySelectorAll('.numpad__number');
 let screen = document.getElementById('display');
+let result = 0;
+let operationBtnPressed = false;
+let currentOperation = '';
 window.onload = function() {
   screen.focus();
  };
@@ -16,14 +19,43 @@ numberButtons.forEach(button => {
 //event listener for the dot
 let dot = document.getElementById('dot');
 dot.addEventListener('click',e=>{
-  console.log(e.target.textContent)
-  console.log(screen.value+e.target.textContent);
   screen.value = String(screen.value)+e.target.textContent
 })
 
 //all other keyboards
 let delBtn = document.getElementById('del');
 delBtn.addEventListener('click', ()=>screen.value="")
+
+//operations object
+
+let arithmetic = {
+  sum: function sum(prev, current){return prev+current},
+  substract: function subs(prev, current){return prev-current},
+  multiply: function multiply(prev, current){return prev*current},
+  divide: function divide(prev, current){return prev/current}
+}
+
+//operations buttons
+let operationBtns = document.querySelectorAll('.operation_btn');
+let operationsArray = ['+','-','X','/'];
+operationBtns.forEach(button=> button.addEventListener('click', e=>{
+  if (!operationBtnPressed){
+    currentOperation = e.target.value;
+    result = Number(screen.value);
+    screen.value = screen.value + ' ' + e.target.textContent + ' ';
+    console.log(currentOperation);
+    operationBtnPressed = true;
+  }else{
+    let newNumber = screen.value.slice(screen.value.indexOf(e.target.textContent,screen.value.length));
+    console.log(newNumber);
+    console.log(currentOperation);
+    screen.value = arithmetic[currentOperation](result,Number(newNumber))
+    result = screen.value;
+    operationBtnPressed = false;
+    currentOperation = ''
+  }
+}));
+
 
 // add event listener to dot and symbols
 const symbols = []
